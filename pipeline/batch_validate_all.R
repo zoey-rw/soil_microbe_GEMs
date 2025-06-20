c85252f92cce1525eb8fe5783a9a0be2394b76a0
Zoey Werbin <zoeywerbin@Zoeys-MacBook-Pro.local>
2025-06-19 23:46:33 -0400

    remove untracked files

--- pipeline/batch_validate_all.R
+++ pipeline/batch_validate_all.R
@@ -13,3 +13,8 @@ validation_results <- run_post_processing_validation("../species/", force_revali
 
 # Create readable report: if models were improved, consistent, or corrupted throughout the pipeline
 report_summary <- create_validation_report("../species")
+
+
+report_summary <- create_validation_report("./carvefungi_species/", output_file = "carvefungi_validation_report.txt")
+
+report_summary <- create_validation_report("./species/", output_file = "curated_model_validation_report.txt")
